68e5bcab7dc605a88a46b503d3d8763d7d75de6a
AsukaKobayashi-agex <kobayashi-asuka@agex.co.jp>
2019-07-18 14:50:42 +0900

    13_02_01.端末一覧:初回構築

---
+++ rental_system/resources/views/rental/admin/Device/index_sp.blade.php
@@ -0,0 +1,122 @@
+@extends('rental.admin.common.include')
+
+@section('content')
+    <!-- Page Heading -->
+    <?php
+    //preDump($all_device_list);
+    $userid = "1";
+    ?>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">モバイル端末一覧</h6>
+        </div>
+        <div class="card-body">
+            <div >
+                <table class="table table-bordered table-striped" id="dataTable" width="100%" cellspacing="0" style="table-layout:fixed;">
+                    <thead>
+                    <tr>
+                        <th width=100px >端末ID</th>
+                        <th>端末名</th>
+                        <th>OS</th>
+                        <th>キャリア</th>
+                    </tr>
+                    </thead>
+
+                    <div name="search_bar" class="mb-3">
+                        @if(count($errors) > 0)
+                            <ul class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
+                        <div class="form-group row">
+                            <form name='search' method="post" action="#">
+                                @csrf
+                                <div class="col-sm-2 mb-4">
+                                    <input type="number" name="search_id" class="form-control form-control-user" value="{{$search_id}}" placeholder="端末IDを入力" >
+                                </div>
+                                <div class="col-sm-2 px-1 ">
+                                    <input type="search" name="search_word" class="form-control form-control-user" value="{{$search_word}}" placeholder="端末名を入力" >
+                                </div>
+                                <div class="col-sm-2 px-1 ">
+                                    <select name="type" class="form-control form-control-user">
+                                        <option value="">カテゴリ</option>
+                                        <option value="1" {{$type==="1" ? 'selected': null}}>スマホ</option>
+                                        <option value="2" {{$type==="2" ? 'selected': null}}>タブレット</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-1 px-1 ">
+                                    <select name="os" class="form-control form-control-user">
+                                        <option value="">OS</option>
+                                        <option value="1" {{($os==="1") ? 'selected': null}}>Android</option>
+                                        <option value="2" {{($os==="2") ? 'selected': null}}>iOS</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-2 px-1 ">
+                                    <input type="number" name="os_version" class="form-control form-control-user" value="{{$os_version}}" placeholder="OSバージョン" >
+                                </div>
+                                <div class="col-sm-2 px-1 ">
+                                    <select name="search_carrier" class="form-control form-control-user">
+                                        <option value="">キャリア</option>
+                                        <option value="1" {{($search_carrier==="1") ? 'selected': null}}>docomo</option>
+                                        <option value="2" {{($search_carrier==="2") ? 'selected': null}}>au</option>
+                                        <option value="3" {{($search_carrier==="3") ? 'selected': null}}>softbank</option>
+                                        <option value="0" {{($search_carrier==="0") ? 'selected': null}}>なし</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-1 px-1">
+                                    <button type="submit" class="btn btn-primary btn-block"><i class="fas fa-fw fa-search"></i>
+                                    </button>
+                                </div>
+                            </form>
+                            <div class="col-sm-2">
+                                <a href="add_sp" class="btn btn-success btn-icon-split float-right">
+                                <span class="icon text-white-50">
+                                  <i class="fas fa-flag"></i>
+                                </span>
+                                    <span class="text">端末を追加する</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    @if(empty($mobile_device_list))
+                        <tr class="font-weight-bold">
+                            <td></td>
+                            <td>一致する項目はありません</td>
+                            <td></td>
+                        </tr>
+                    @endif
+
+                    <tbody>@foreach($mobile_device_list as $device)
+                        <tr class="font-weight-bold">
+                            <td>
+                                <?=$device['rental_device_id']?>
+                            </td>
+                            <td>
+                                <a class="text-lg" target="_blank" href="/detail-mobile?rental_device_id=<?=$device['rental_device_id']?>" ><?=$device['device_name']?></a>
+                            </td>
+                            <td> @if($device['os']==1)
+                                    Android
+                                @elseif($device['os']==2)
+                                    iOS
+                                @else
+                                    Other OS
+                                @endif
+                                <?=$device['os_version']?>
+                            </td>
+                            <td>
+                                <?=$device['carrier_name']?>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
