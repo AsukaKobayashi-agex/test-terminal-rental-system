1abbcd28ea2aa9ec69684dea9d9f7763f98df6dd
yamane mizuki <yamane-mizuki@agex.co.jp>
2019-07-24 11:45:24 +0900

    構文の修正

--- rental_system/Packages/Rental/Models/_common/MobileInstalledAppData.php
+++ rental_system/Packages/Rental/Models/_common/MobileInstalledAppData.php
@@ -4,17 +4,19 @@ namespace Rental\Models\_common;
 
 class MobileInstalledAppData
 {
-    public function insertMobileInstalledApp($param)
+    public function insertMobileInstalledApp($test_device_id,$param)
     {
         $now = nowDateTime();
 
-        foreach($param['software_id'] as $mobile_app_id){
-            $insert_data[] = [
-                'test_device_id' =>$param['test_device_id'],
+        foreach($param['mobile_app_id'] as $mobile_app_id){
+            $insert_data = [
+                'test_device_id' =>$test_device_id,
                 'mobile_app_id' =>$mobile_app_id,
                 'add_date' =>$now
             ];
+
+            \DB::table('mobile_installed_app')->insert($insert_data);
         }
-        return \DB::table('mobile_installed_app')->insert($insert_data);
+        return true;
     }
 }
