de1fb3b7d2611dc234933bf00c328fb591d1137c
yamane mizuki <yamane-mizuki@agex.co.jp>
2019-07-29 14:57:11 +0900

    一括バーのスタイル変更

--- rental_system/resources/views/rental/user/common/bundle_bar.blade.php
+++ rental_system/resources/views/rental/user/common/bundle_bar.blade.php
@@ -1,13 +1,13 @@
 <div id="bundle_bar" class="form-group row float-right">
     <form method="post" id="bundle">
         @csrf
-        <div class="mx-2">
+        <div class="col-md-4">
             <button type="submit" class="btn btn-primary btn-user btn-block bundle px-5" disabled="disabled" formaction="/rental">一括貸出</button>
         </div>
-        <div class="mx-2">
+        <div class="col-md-4">
             <button type="submit" class="btn btn-danger btn-user btn-block bundle px-5" disabled="disabled" formaction="/return">一括返却</button>
         </div>
-        <div class="mx-3">
+        <div class="col-md-4">
             <button type="submit" class="btn btn-success btn-user btn-block bundle px-4" disabled="disabled" formaction="/mylist-register">マイリスト登録</button>
         </div>
     </form>
