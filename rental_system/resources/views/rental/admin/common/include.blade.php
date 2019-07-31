d20ad6e8f8c97e01b7753f5025cb26e9a8cc759f
yamane mizuki <yamane-mizuki@agex.co.jp>
2019-07-31 12:54:02 +0900

    親ブレードをパーツごとに分けてインクルード

--- rental_system/resources/views/rental/admin/common/include.blade.php
+++ rental_system/resources/views/rental/admin/common/include.blade.php
@@ -1,181 +1,31 @@
 <!DOCTYPE html>
 <html lang="ja">
 
-<head>
+@include('rental.admin.common.header')
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>@yield('title')[管理者]テスト端末貸出システム</title>
-
-
-    <!-- Custom fonts for this template -->
-    <link href="/bootsample/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="/bootsample/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="/css/admin/admin_base.css" rel="stylesheet">
-
-    <!-- Custom styles for this page -->
-    <link href="/bootsample/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-
-
-
-</head>
-
-
-<body id="page-top">
+<body id="page-top" class="{{$_COOKIE['sideOpen'] ? 'sidebar-toggled':null}}">
 
 <!-- Page Wrapper -->
 <div id="wrapper">
 
-    <!-- Sidebar -->
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-        <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/admin/index_all">
-            <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-laugh-wink"></i>
-            </div>
-            <div class="sidebar-brand-text mx-3">管理者画面</div>
-        </a>
-
-        <hr class="sidebar-divider">
-
-
-        <!-- Nav Item - Tables -->
-        <li {!! (Request::is('admin/index_all') ? 'class="nav-item active"' : 'class="nav-item"') !!}>
-            <a class="nav-link" href="/admin/index_all">
-                <i class="fas fa-fw fa-table"></i>
-                <span>端末一覧</span></a>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider">
-
-
-
-        <div class="sidebar-heading">
-            カテゴリ別
-        </div>
-
-        <li  {!! (Request::is('admin/index_sp') ? 'class="nav-item active"' : 'class="nav-item"') !!}>
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                <i class="fas fa-fw fa-mobile"></i>
-                <span >モバイル</span>
-            </a>
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <form method="get" name="ios" action="/admin/index_sp">
-                        <a class="collapse-item text-primary" href="/admin/index_sp">モバイル</a>
-                    </form>
-                    <form method="get" name="smartphone" action="/admin/index_sp">
-                        <input type="hidden" name="type" value="1">
-                        <a class="collapse-item text-center" href="javascript:smartphone.submit()">スマホ</a>
-                    </form>
-                    <form method="get" name="tablet" action="/admin/index_sp">
-                        <input type="hidden" name="type" value="2">
-                        <a class="collapse-item text-center" href="javascript:tablet.submit()">タブレット</a>
-                    </form>
-                </div>
-            </div>
-        </li>
-
-        <li  {!! (Request::is('admin/index_pc') ? 'class="nav-item active"' : 'class="nav-item"') !!}>
-            <a class="nav-link" href="/admin/index_pc">
-                <i class="fas fa-fw fa-laptop"></i>
-                <span>PC</span>
-            </a>
-        </li>
-        <li  {!! (Request::is('admin/index_charger') ? 'class="nav-item active"' : 'class="nav-item"') !!}>
-            <a class="nav-link" href="/admin/index_charger">
-                <i class="fas fa-fw fa-charging-station"></i>
-                <span>充電器</span>
-            </a>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
-
-        <li  {!! (Request::is('admin/master') ? 'class="nav-item active"' : 'class="nav-item"') !!}>
-            <a class="nav-link" href="/admin/master">
-                <i class="fas fa-fw fa-list"></i>
-                <span>マスター管理</span>
-            </a>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
-
-        <!-- Sidebar Toggler (Sidebar) -->
-        <!--<div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-        </div>-->
-
-    </ul>
-    <!-- End of Sidebar -->
-
+    @include('rental.admin.common.sidebar')
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
         <!-- Main Content -->
         <div id="content">
 
-            <!-- Topbar -->
-            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                <!-- Sidebar Toggle (Topbar) -->
-                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                    <i class="fa fa-bars"></i>
-                </button>
-
-                <ul class="navbar-nav ml-auto">
-                    <div class="d-flex align-items-center ">
-                        @php(date_default_timezone_set('Asia/Tokyo'))
-                        <h4 class="text-lg-left text-primary"><?=date('Y/m/d D H:i')?></h4>
-                    </div>
-
-                    <div class="topbar-divider d-none d-sm-block"></div>
-
-                <!-- Topbar Navbar -->
-                <ul class="navbar-nav ml-auto">
-
-
-
-
-
-                    <!-- Nav Item - User Information -->
-                    @if(\Auth::guard('admin')->check())
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 text-gray-900">{{$admin_info['name']}}</span>
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    ログアウト
-                                </a>
-                            </div>
-                        </li>
-                    @else
-                        <a class="nav-link" href="/login">
-                            <span class="mr-2 text-gray-900">ログイン</span>
-                        </a>
-                    @endif
-
-                </ul>
-
-            </nav>
-            <!-- End of Topbar -->
+            @include('rental.admin.common.topbar')
 
             <!-- Begin Page Content -->
             <div class="container-fluid">
-                @section('content')
+                @if (session('success'))
+                    <div class="alert alert-success text-center">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+            @section('content')
                 @show
             </div>
 
@@ -184,15 +34,7 @@
         </div>
         <!-- End of Main Content -->
 
-        <!-- Footer -->
-        <footer class="sticky-footer bg-white">
-            <div class="container my-auto">
-                <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Your Website 2019</span>
-                </div>
-            </div>
-        </footer>
-        <!-- End of Footer -->
+        @include('rental.admin.common.footer')
     </div>
     <!-- End of Content Wrapper -->
 
@@ -223,18 +65,8 @@
         </div>
     </div>
 </div>
-<!-- コアスクリプト-->
-<!-- Bootstrap core JavaScript-->
-<script src="/bootsample/vendor/jquery/jquery.min.js"></script>
-<script src="/bootsample/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<!-- Core plugin JavaScript-->
-<script src="/bootsample/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="/bootsample/js/sb-admin-2.min.js"></script>
-
-@stack('scripts')
+@include('rental.admin.common.core_script')
 
 </body>
 
