6b7e43d04db129035588eab155220dbdf25834d7
yamane mizuki <yamane-mizuki@agex.co.jp>
2019-08-01 16:09:41 +0900

    firefoxでのバンドルバーの表示崩れを修正

--- rental_system/resources/views/rental/user/common/bundle_bar.blade.php
+++ rental_system/resources/views/rental/user/common/bundle_bar.blade.php
@@ -1,13 +1,13 @@
 <div id="bundle_bar" class="form-group row float-right">
     <form method="post" id="bundle">
         @csrf
-        <div class="col-md-4">
+        <div class="mx-2 mb-sm-0 mb-3">
             <button type="submit" class="btn btn-primary btn-user btn-block bundle px-5" disabled="disabled" formaction="/rental">一括貸出</button>
         </div>
-        <div class="col-md-4">
+        <div class="mx-2 mb-sm-0 mb-3">
             <button type="submit" class="btn btn-danger btn-user btn-block bundle px-5" disabled="disabled" formaction="/return">一括返却</button>
         </div>
-        <div class="col-md-4">
+        <div class="mx-2 mb-sm-0 mb-3">
             <button type="submit" class="btn btn-success btn-user btn-block bundle px-4" disabled="disabled" formaction="/mylist-register">マイリスト登録</button>
         </div>
     </form>
