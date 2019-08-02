a244ab2906e25701ebb187ed80561c07e5fda55c
AsukaKobayashi-agex <kobayashi-asuka@agex.co.jp>
2019-08-02 10:36:55 +0900

    最終貸出人を表示させないようにしました

--- rental_system/Packages/Rental/Services/Admin/Info/InfoSpService.php
+++ rental_system/Packages/Rental/Services/Admin/Info/InfoSpService.php
@@ -4,7 +4,7 @@ namespace Rental\Services\Admin\Info;
 
 use Rental\Models\_common\MobileAppMasterData;
 use Rental\Models\_common\MobileCarrierData;
-use Rental\Models\_common\RentalHistoryData;
+//use Rental\Models\_common\RentalHistoryData;
 use Rental\Models\Admin\Info\InfoSpData;
 
 class InfoSpService
@@ -14,12 +14,12 @@ class InfoSpService
     protected $_mobile_carrier;
     protected $_history;
 
-    public function __construct(InfoSpData $model,MobileAppMasterData $mobile_app_master,MobileCarrierData $mobile_carrier,RentalHistoryData $history)
+    public function __construct(InfoSpData $model,MobileAppMasterData $mobile_app_master,MobileCarrierData $mobile_carrier/*,RentalHistoryData $history*/)
     {
         $this->_model = $model;
         $this->_mobile_app_master = $mobile_app_master;
         $this->_mobile_carrier = $mobile_carrier;
-        $this->_history = $history;
+        // $this->_history = $history;
     }
 
     public function getData($param)
@@ -31,7 +31,8 @@ class InfoSpService
         $data['detail']['admin_memo'] = nl2br($data['detail']['admin_memo']);
         $data['mobile_carrier'] = $this->_mobile_carrier->getAll();
         $data['mobile_app_master'] = $this->_mobile_app_master->getAll();
-        $data['recent_user'] = $this->_history->getOne($param['rental_device_id']);
+        //$data['recent_user'] = $this->_history->getOne($param['rental_device_id']);
+        $data['recent_user'] = '';
         if(\Auth::guard('admin')->check()) {
             $data['admin_info'] = $this->_model->getAdminAccountData();
         }
