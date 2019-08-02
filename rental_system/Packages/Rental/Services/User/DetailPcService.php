d69474f42fb92332a748b8072da3cbe0ef1d79c1
AsukaKobayashi-agex <kobayashi-asuka@agex.co.jp>
2019-08-02 10:42:24 +0900

    最終貸出人を表示させないようにしました

--- rental_system/Packages/Rental/Services/User/DetailPcService.php
+++ rental_system/Packages/Rental/Services/User/DetailPcService.php
@@ -2,7 +2,7 @@
 
 namespace Rental\Services\User;
 
-use Rental\Models\_common\RentalHistoryData;
+//use Rental\Models\_common\RentalHistoryData;
 use Rental\Models\User\DetailPcData;
 
 class DetailPcService
@@ -10,10 +10,10 @@ class DetailPcService
     protected $_model;
     protected $_history;
 
-    public function __construct(DetailPcData $model,RentalHistoryData $history)
+    public function __construct(DetailPcData $model/*,RentalHistoryData $history*/)
     {
         $this->_model = $model;
-        $this->_history = $history;
+     //   $this->_history = $history;
     }
 
     public function getData($param)
@@ -22,7 +22,8 @@ class DetailPcService
         $data['detail'] = $this->_model->getAllDetailPc($param);
         $data['detail']['memo'] = nl2br($data['detail']['memo']);
         $data['installed_software_list'] = $this->_model->getAllInstalledSoftware($param);
-        $data['recent_user'] = $this->_history->getOne($param['rental_device_id']);
+       // $data['recent_user'] = $this->_history->getOne($param['rental_device_id']);
+        $data['recent_user'] = '';
 
         return $data;
     }
